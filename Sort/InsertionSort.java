52cb4acc44c829b0d3ba4b08033077468e12cd0e
Артур Алтынчурин <alturik9@yandex.ru>
2023-07-31 21:42:27 +0500

    sort

---
+++ Sort/InsertionSort.java
@@ -0,0 +1,20 @@
+package HW1.Sort;
+
+public class InsertionSort {
+    
+    public InsertionSort() {    
+    }
+
+    public void sort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+}
